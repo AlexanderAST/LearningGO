749c691b67d5e5b19ce8523e0fa493a8e76e1fdd
AlexanderAST <89164909109@bk.ru>
2022-08-23 20:27:57 +0300

    second commit

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
+func home_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Eto shto moi sait ashalet`")
+}
+func contacts_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Vtoraa stranica OOOOOOOOOOO")
+}
+func handleRequest() {
+	http.HandleFunc("/", home_page)
+	http.HandleFunc("/contacts/", contacts_page)
+	http.ListenAndServe(":7777", nil)
+}
 func main() {
-	fmt.Println("Hello world")
-	fmt.Println("Hello go")
+	handleRequest()
 }
