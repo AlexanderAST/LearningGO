0fd1f9142d6c59603c27a1174744daa0eb5d8554
AlexanderAST <89164909109@bk.ru>
2022-08-24 12:04:21 +0300

    3 tutorial,classes and links

--- main.go
+++ main.go
@@ -5,8 +5,25 @@ import (
 	"net/http"
 )
 
+type User struct {
+	name                  string
+	age                   uint16
+	money                 int16
+	ave_grades, happiness float64
+}
+
+func (u User) getAllinfo() string {
+	return fmt.Sprintf("User name is: %s. He is %d and he has money: %d рублей", u.name, u.age, u.money)
+}
+func (u *User) setNewname(newName string) {
+	u.name = newName
+}
+
 func home_page(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Eto shto moi sait ashalet`")
+	john := User{name: "John", age: 18, money: 5, ave_grades: 5.5, happiness: 100}
+	//fmt.Fprintf(w, "Eto shto moi sait ashalet`")
+	john.setNewname("Sanya")
+	fmt.Fprintf(w, john.getAllinfo())
 }
 func contacts_page(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Vtoraa stranica OOOOOOOOOOO")
@@ -18,4 +35,7 @@ func handleRequest() {
 }
 func main() {
 	handleRequest()
+	//var John User =... есть более простой
+	//john := User{name:"John", age: 44, money: 14325,ave_grades: 5.5, happiness: 100}
+
 }
